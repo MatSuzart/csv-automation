5b5081e50152c424516395a930359807a1d2702e
MatheusSuzart <mathsuzart@outlook.com>
2023-03-31 23:06:33 -0300

    mysql2-import

--- src/inde.js
+++ src/inde.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const csv = require('csv-parse');
 const fs = require('fs');
-//const pool require('mysql');
+const mysql = require('mysql2/promise');
+
 
 const app = express();
 
@@ -14,3 +15,10 @@ app.listen(port, ()=>{
 app.get('/', (req, res)=>{
     res.send('response');
 });
+
+const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: ''
+  });
